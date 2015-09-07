0b53e23b87f8cf98ee9b76220369ed3f149b4a9a
Masaki Kagaya <masakielastic@gmail.com>
2015-09-07 21:47:12 +0900

    inital commit

---
+++ HtmlGeneratorServiceProvider.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Masakielastic\Silex;
+
+use Silex\Application;
+use Silex\ServiceProviderInterface;
+use Symfony\Component\HttpFoundation\Request;
+
+class HtmlGeneratorServiceProvider implements ServiceProviderInterface
+{
+    public function register(Application $app)
+    {
+        $app['htmlgen.baseUri'] = '/';
+        $app['htmlgen.content.dir'] = __DIR__.'/content'; 
+        $app['htmlgen.public.dir'] = __DIR__.'/public';
+
+        $app['htmlgen'] = $app->protect(function () use ($app) {
+
+            $it = new DirectoryIterator($app['htmlgen.content.dir']);
+
+            foreach ($it as $file) {
+
+                if ($file->isDir()) {
+                    continue;
+                }
+
+                $name = $file->getBasename('.md');
+                $path = $app['htmlgen.baseUri'].$name;
+                $response = $app->handle(Request::create($path));
+                $content = $response->getContent();
+
+                file_put_contents($app['htmlgen.public.dir']."/$name.html", $content);
+            }
+        });
+    }
+
+    public function boot(Application $app)
+    {
+    }
+}
